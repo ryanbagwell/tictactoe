dd4af6f8264d8c7a17247f1493b6dd4de011fde6
Ryan Bagwell <ryan@ryanbagwell.com>
2013-12-29 12:54:39 -0500

    big fixes based on new tests

--- tictactoe/apps/main/game.py
+++ tictactoe/apps/main/game.py
@@ -5,40 +5,44 @@ import json
 
 game_cache = get_cache('default')
 
-class TicTacToe(object):
+class TicTacToeGame(object):
+    board = None
+    game_id = None
+
 
     def __init__(self, game_id=None):
 
         if game_id:
-            self.game = self.get_game(game_id)
+            self.board = self.get_game(game_id)
         else:
-            self.game = self.get_new_game()
+            self.board = self.get_new_game()
 
 
     def get_game(self, game_id):
         game = game_cache.get(game_id)
-        return json.loads(game)
+        self.game_id = game_id
+        return game
 
 
     def get_new_game(self):
 
-        """ Create an empty dictionary with 9 places for
-            our moves """
-        game = dict((v, '') for v in range(9))
+        """ Create an empty dictionary with 9 places to
+            represent our board """
+        board = dict((v, '') for v in range(9))
 
         """ Pick a corner for the computer's first move """
-        start = random.choice('0,2,6,8')
+        start = random.choice([0,2,6,8])
 
         """ Then mark the corner as played """
-        self.game[start] = 'x'
+        board[start] = 'x'
 
         """ Generate a random id to assign to our game """
-        game_id = uuid.uuid1().hex
+        self.game_id = uuid.uuid1().hex
 
         """ Place it in memory and return it """
-        game_cache.set(game_id, self.game)
+        game_cache.set(self.game_id, board)
 
-        return game
+        return board
 
 
 
