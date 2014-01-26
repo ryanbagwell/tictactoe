90efad5f649124a306aa0e21bc9db875208d0ef2
Ryan Bagwell <ryan@ryanbagwell.com>
2014-01-26 13:50:37 -0600

    renaming resource

--- tictactoe/apps/main/urls.py
+++ tictactoe/apps/main/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^game/$', NewGameView.as_view(), name="new-game"),
-    url(r'^game/(?P<game_id>[^/]*)/$', RetrieveGameView.as_view(), name="retrieve-game"),
+    url(r'^game/(?P<game_id>[^/]*)/$', ExistingGameView.as_view(), name="existing-game"),
     url(r'^game/(?P<game_id>[^/]*)/?(?P<method>[^/]*)/?$', GameOperationView.as_view(), name="game-operation"),
     url(r'^', HomeView.as_view(), name="home"),
 )
